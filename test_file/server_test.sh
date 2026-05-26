27fff4bcce35c34f35d202c39e866746b0fa19ed
oyouki <90975149+oyouki@users.noreply.github.com>
2026-05-27 04:01:04 +0900

    Delete test_file directory

--- test_file/server_test.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-
-cd ~/program/
-cp server.c server_test.c
-cp db.c db_test.c
-gcc -g -o db.out db_test.c ser_cli_std.c -lssl -lcrypto -lmysqlclient
-gcc -g -o server.out server_test.c ser_cli_std.c -lssl -lcrypto
-gdb server.out
--- test_file/tool_test.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-
-cd ~/program/
-cp tool.c tool_test.c
-gcc -g -o tool_test.out tool_test.c
-gdb tool_test.out
